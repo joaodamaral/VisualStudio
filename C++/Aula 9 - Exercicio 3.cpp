acd416bac45c0b902cd8bbac208beb0bddabfcc5
joaodamaral <joaodamaral@hotmail.com>
2022-04-05 20:49:18 -0300

    Aula 9

---
+++ C++/Aula 9 - Exercicio 3.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <math.h>
+using namespace std;
+
+main(){
+
+    int valor, i;
+
+    cout << "Insira o valor\n";
+    cin >> valor;
+    i = 1;
+    
+    do{
+        cout << i << " " << pow(i, 2) << " " << pow(i, 3) << endl;
+        cout << i << " " << pow(i, 2) + 1 << " " << pow(i, 3) + 1 << endl;
+        i++;
+    }while (i <= valor);
+
+    /*Final dos códigos para rodar no Windows 11 Visual Studio (Previne fechamento do prompt, após fim do programa*/
+	cout << "\n" << endl;
+	system("pause");
+	return 0;
+}
