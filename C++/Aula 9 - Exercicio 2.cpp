ad845aa10f462fce2b249b98c0c51727c6948601
joaodamaral <joaodamaral@hotmail.com>
2022-04-06 11:34:54 -0300

    Aula 9

---
+++ C++/Aula 9 - Exercicio 2.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+using namespace std;
+
+main(){
+    
+    float n1, n2, inv1, inv2;
+    char repetidor;
+
+do{
+    do{
+        cout << "Insira a primeira nota\n";
+        cin >> n1;
+
+        if (n1 < 0){
+            cout << "Nota Invalida\n";
+            inv1 = 1;
+        }
+        else{
+            inv1 = 0;
+        }
+
+    }while (inv1 != 0);
+
+
+    do{
+        cout << "Insira a segunda nota\n";
+        cin >> n2;
+
+        if (n2 < 0){
+            cout << "Nota Invalida\n";
+            inv2 = 1;
+        }
+        else{
+            inv2 = 0;
+        }
+    }while (inv2 != 0);
+
+    cout << "media = " << (n1+n2)/2 << endl;
+    cout << "Novo calculo? (y/n)\n";
+    cin >> repetidor;
+
+}while (repetidor == 'y');
+
+
+    /*Final dos códigos para rodar no Windows 11 Visual Studio (Previne fechamento do prompt, após fim do programa*/
+	cout << "\n" << endl;
+	system("pause");
+	return 0;
+}
